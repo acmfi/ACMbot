3af6e2957648b1b1b25bb77c7d919743f173a12d
Ironjanowar <ironjanowar@gmail.com>
2015-12-17 12:37:59 +0100

    Función de dar información creada y funcionando

--- bot.py
+++ bot.py
@@ -2,12 +2,17 @@ import telebot
 
 bot = telebot.TeleBot("92880306:AAHx-oybgswhlloA_hFxpBBvknBltoAyaV0")
 
+with open('./data/QueHaceACM.txt', 'r') as myfile:
+  info = myfile.read().replace('\n', '')
+
+print("Running...")
+
 @bot.message_handler(commands=['start', 'help'])
 def send_welcome(message):
-    bot.reply_to(message, "¡Hola! Te damos al bienvenida al bot de ACM.")
+  bot.reply_to(message, "¡Hola! Te damos al bienvenida al bot de ACM.")
 
-@bot.message_handler(func=lambda message: True)
-def echo_all(message):
-    bot.reply_to(message, message.text)
+@bot.message_handler(commands=['queHaceACM'])
+def send_info(message):
+  bot.reply_to(message, info)
     
 bot.polling()
