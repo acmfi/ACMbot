2167b39e56cedc58e41713bd9feb82a7a1291d98
Ignacio Ballesteros <ignacioballesterosgonzalez@gmail.com>
2015-12-17 16:12:50 +0100

    Actualizados los mensajes de bienvenida y sobre conocimiento

--- bot.py
+++ bot.py
@@ -2,14 +2,17 @@ import telebot
 
 bot = telebot.TeleBot("92880306:AAHx-oybgswhlloA_hFxpBBvknBltoAyaV0")
 
-with open('./data/QueHaceACM.txt', 'r') as myfile:
-  info = myfile.read()
+with open('./data/QueHaceACM.txt', 'r') as knowUs_file:
+  info = knowUs_file.read()
 
+with open('./data/bienvenida.txt', 'r') as welcome_file:
+  welcome = welcome_file.read()
+  
 print("Running...")
 
 @bot.message_handler(commands=['start', 'help'])
 def send_welcome(message):
-  bot.reply_to(message, "¡Hola! Te damos al bienvenida al bot de ACM.")
+  bot.reply_to(message, welcome)
 
 @bot.message_handler(commands=['quehaceacm'])
 def send_info(message):
