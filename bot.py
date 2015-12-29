089e0a4028f46650802229032775068bd11966d6
Ironjanowar <ironjanowar@gmail.com>
2015-12-29 23:34:10 +0100

    JSON stylah

--- bot.py
+++ bot.py
@@ -1,13 +1,13 @@
 import telebot
+import json
 
 bot = telebot.TeleBot("92880306:AAHx-oybgswhlloA_hFxpBBvknBltoAyaV0")
 
-with open('./data/QueHaceACM.txt', 'r') as knowUs_file:
-  info = knowUs_file.read()
+with open('./data/data.json', 'r') as data:
+  j = json.load(data)
+  info = j['info']
+  welcome = j['bienvenida']
 
-with open('./data/bienvenida.txt', 'r') as welcome_file:
-  welcome = welcome_file.read()
-  
 print("Running...")
 
 @bot.message_handler(commands=['start', 'help'])
