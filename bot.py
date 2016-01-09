89a3a8d1dd39fca78337b5e23db416b9d3a79948
Ironjanowar <ironjanowar@gmail.com>
2016-01-09 12:45:01 +0100

    Funcion que envia la lista de la comida añadida

--- bot.py
+++ bot.py
@@ -1,7 +1,8 @@
 import telebot
 import json
+from telebot import types
 
-bot = telebot.TeleBot("92880306:AAHx-oybgswhlloA_hFxpBBvknBltoAyaV0")
+bot = telebot.TeleBot("92880306:AAFmwCI80R60hhHRcGNDl8m7Z-Oz5e_GqXE")
 
 with open('./data/data.json', 'r') as data:
   j = json.load(data)
@@ -17,5 +18,10 @@ def send_welcome(message):
 @bot.message_handler(commands=['quehaceacm'])
 def send_info(message):
   bot.reply_to(message, info)
-    
+
+@bot.message_handler(commands=['comida'])
+def send_precios(message):
+  photo = open('./data/listaComida.jpg', 'rb')
+  bot.send_photo(message.chat.id, photo)
+  
 bot.polling()
