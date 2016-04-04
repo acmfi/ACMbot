c28850a8d8274f60ac88abff67acb537afd6df11
ironjanowar <ironjanowar@gmail.com>
2016-04-04 21:19:29 +0200

    Python indent and format :poop:

--- bot.py
+++ bot.py
@@ -1,88 +1,92 @@
 import sys
 import telebot
 import json
-import subprocess as sub
-#from TwitterAPI import TwitterAPI
+# from TwitterAPI import TwitterAPI
 from telebot import types
 
 # Create bot with its token
-
-with open ("./acm.token","r") as TOKEN:
-  bot = telebot.TeleBot(TOKEN.readline().strip())
+with open("./acm.token", "r") as TOKEN:
+    bot = telebot.TeleBot(TOKEN.readline().strip())
 
 # Ignorar mensajes antiguos
 bot.skip_pending = True
 
 # Authenticate on Twitter (@acmupm)
-#with open ("./data/.twitter.json") as twitter:
-#  t = json.load(twitter)
-#  consumerKey = t['ConsumerK']
-#  consumerSecret = t['ConsumerS']
-#  accessToken = t['AccessT']
-#  accessSecret = t['AccessS']
+# with open ("./data/.twitter.json") as twitter:
+#   t = json.load(twitter)
+#   consumerKey = t['ConsumerK']
+#   consumerSecret = t['ConsumerS']
+#   accessToken = t['AccessT']
+#   accessSecret = t['AccessS']
 
 # Vars used
 preciosTracking = {}
 lmgtfyTracking = {}
 
 # Functions used
+
+
 def isAdmin_fromPrivate(message):
-  if message.chat.type == 'private':
-    userID = message.from_user.id
-    if str(userID) in admins:
-      return True
-  return False
+    if message.chat.type == 'private':
+        userID = message.from_user.id
+        if str(userID) in admins:
+            return True
+        return False
+
 
 def isUserAnswer(user, userTracking):
-  if user in userTracking.keys():
-    return True
-  else:
-    return False
+    if user in userTracking.keys():
+        return True
+    else:
+        return False
+
 
 def listener(messages):
-  # When new messages arrive TeleBot will call this function.
-  for m in messages:
-    if m.content_type == 'text':
-      # Prints the sent message to the console
-      if m.chat.type == 'private':
-        print ("Chat -> " + str(m.chat.first_name) + " [" + str(m.chat.id) + "]: " + m.text)
-      else:
-        print ("Group -> " + str(m.chat.title) + " [" + str(m.chat.id) + "]: " + m.text)
-    
+    # When new messages arrive TeleBot will call this function.
+    for m in messages:
+        if m.content_type == 'text':
+            # Prints the sent message to the console
+            if m.chat.type == 'private':
+                print("Chat -> " + str(m.chat.first_name) +
+                      " [" + str(m.chat.id) + "]: " + m.text)
+        else:
+            print("Group -> " + str(m.chat.title) +
+                  " [" + str(m.chat.id) + "]: " + m.text)
+
 # Initializing listener
-bot.set_update_listener(listener) 
+bot.set_update_listener(listener)
 
 # Twitter login
-#api = TwitterAPI(consumerKey, consumerSecret, accessToken, accessSecret)
+# api = TwitterAPI(consumerKey, consumerSecret, accessToken, accessSecret)
 
 # Files used
 with open('./data/data.json', 'r') as data:
-  j = json.load(data)
-  info = j['info']
-  welcome = j['bienvenida']
-  events = j['events']
-  reto = j['reto']
-  bebida = j['bebida']
-  comida = j['comida']
-  especiales = j['especiales']
+    j = json.load(data)
+    info = j['info']
+    welcome = j['bienvenida']
+    events = j['events']
+    reto = j['reto']
+    bebida = j['bebida']
+    comida = j['comida']
+    especiales = j['especiales']
 
 with open('./data/help.json', 'r') as leHelp:
-  helpData = json.load(leHelp)
+    helpData = json.load(leHelp)
 
 helpMessage = "Estos son los comandos disponibles:\n\n"
 for key in helpData:
-  helpMessage += "- /" + key + " :: "
-  helpMessage += helpData[key] + "\n"
+    helpMessage += "- /" + key + " :: "
+    helpMessage += helpData[key] + "\n"
 
 with open('./data/admins.json', 'r') as adminData:
-  admins = json.load(adminData)
+    admins = json.load(adminData)
 
 print("Running...")
 
 # Custom keyboards
 seleccionComida = types.ReplyKeyboardMarkup(one_time_keyboard=True)
-seleccionComida.add('Bebida','Especiales')
-seleccionComida.add('Comida','Todo')
+seleccionComida.add('Bebida', 'Especiales')
+seleccionComida.add('Comida', 'Todo')
 seleccionComida.add('Cerrar')
 
 lmgtfySearch = types.ReplyKeyboardMarkup(one_time_keyboard=True)
@@ -91,110 +95,124 @@ lmgtfySearch.add('Cancelar')
 hideBoard = types.ReplyKeyboardHide()
 
 # Handlers
+
+
 @bot.message_handler(commands=['start'])
 def send_welcome(message):
-  bot.reply_to(message, welcome)
+    bot.reply_to(message, welcome)
+
 
 @bot.message_handler(commands=['help'])
 def send_help(message):
-  bot.reply_to(message, helpMessage)
+    bot.reply_to(message, helpMessage)
+
 
 @bot.message_handler(commands=['quehaceacm'])
 def send_info(message):
-  bot.reply_to(message, info)
+    bot.reply_to(message, info)
+
 
 @bot.message_handler(commands=['precios'])
 def send_precios_comida(message):
-  chatId = message.chat.id
-  bot.send_message(chatId, "Que precios quieres ver?", reply_markup=seleccionComida)
-  preciosTracking[message.from_user.id] = message.chat.first_name
+    chatId = message.chat.id
+    bot.send_message(chatId, "Que precios quieres ver?",
+                     reply_markup=seleccionComida)
+    preciosTracking[message.from_user.id] = message.chat.first_name
+
 
 @bot.message_handler(func=lambda message: isUserAnswer(message.from_user.id, preciosTracking))
 def msg_seleccion_precio(message):
- chatId = message.chat.id
- text = message.text
- if text == 'Bebida':
-   bot.send_message(chatId, bebida, reply_markup=hideBoard)
- elif text == 'Comida':
-   bot.send_message(chatId, comida, reply_markup=hideBoard)
- elif text == 'Especiales':
-   bot.send_message(chatId, especiales, reply_markup=hideBoard)
- elif text == 'Todo':
-   photo = open('./data/listaComida.jpg', 'rb')
-   bot.send_photo(message.chat.id, photo, reply_markup=hideBoard)
- else:
-   bot.send_message(chatId, "#sexyACM", reply_markup=hideBoard)
+    chatId = message.chat.id
+    text = message.text
+    if text == 'Bebida':
+        bot.send_message(chatId, bebida, reply_markup=hideBoard)
+    elif text == 'Comida':
+        bot.send_message(chatId, comida, reply_markup=hideBoard)
+    elif text == 'Especiales':
+        bot.send_message(chatId, especiales, reply_markup=hideBoard)
+    elif text == 'Todo':
+        photo = open('./data/listaComida.jpg', 'rb')
+        bot.send_photo(message.chat.id, photo, reply_markup=hideBoard)
+    else:
+        bot.send_message(chatId, "#sexyACM", reply_markup=hideBoard)
 
- preciosTracking.pop(message.from_user.id, None)
+    preciosTracking.pop(message.from_user.id, None)
+
 
 @bot.message_handler(commands=['eventos'])
 def send_events(message):
-  if events == '':
-    GIF = open('./data/carlton.mp4', 'rb')
-    bot.send_message(message.chat.id, 'No hay eventos!')
-    bot.send_document(message.chat.id, GIF)
-  else:
-    bot.reply_to(message, events)
+    if events == '':
+        GIF = open('./data/carlton.mp4', 'rb')
+        bot.send_message(message.chat.id, 'No hay eventos!')
+        bot.send_document(message.chat.id, GIF)
+    else:
+        bot.reply_to(message, events)
+
 
 @bot.message_handler(commands=['reto'])
 def send_challenge(message):
-  bot.reply_to(message, "El reto de esta semana es:\n\n" + reto)
+    bot.reply_to(message, "El reto de esta semana es:\n\n" + reto)
+
 
 @bot.message_handler(commands=['lmgtfy'])
 def send_lmgtfy(message):
-  if message.text == "/lmgtfy" or message.text == "/lmgtfy@acmupm_bot":
-    bot.send_message(message.chat.id, "Que quieres que busque por ti?", reply_markup=lmgtfySearch)
-    lmgtfyTracking[message.from_user.id] = message.chat.first_name
-  else:
-    lmgtfy_url = "http://lmgtfy.com/?q=" + "+".join(message.text.split()[1:])
-    bot.reply_to(message, lmgtfy_url)
+    if message.text == "/lmgtfy" or message.text == "/lmgtfy@acmupm_bot":
+        bot.send_message(message.chat.id, "Que quieres que busque por ti?",
+                         reply_markup=lmgtfySearch)
+        lmgtfyTracking[message.from_user.id] = message.chat.first_name
+    else:
+        lmgtfy_url = "http://lmgtfy.com/?q=" + "+".join(message.text.split()[1:])
+        bot.reply_to(message, lmgtfy_url)
+
 
 @bot.message_handler(func=lambda message: isUserAnswer(message.from_user.id, lmgtfyTracking))
 def send_lmgtfyExtended(message):
-  if message.text == 'Cancelar':
-    bot.send_message(message.chat.id, "#sexyACM", reply_markup=hideBoard)
-  else:
-    lmgtfy_url = "http://lmgtfy.com/?q=" + "+".join(message.text.split())
-    bot.reply_to(message, lmgtfy_url, reply_markup=hideBoard)
+    if message.text == 'Cancelar':
+        bot.send_message(message.chat.id, "#sexyACM", reply_markup=hideBoard)
+    else:
+        lmgtfy_url = "http://lmgtfy.com/?q=" + "+".join(message.text.split())
+        bot.reply_to(message, lmgtfy_url, reply_markup=hideBoard)
 
-  lmgtfyTracking.pop(message.from_user.id, None)
-  
+    lmgtfyTracking.pop(message.from_user.id, None)
+
+
 @bot.message_handler(commands=['tldr'])
 def send_tldr(message):
-  GIF = open('./data/tldr.mp4', 'rb')
-  bot.send_document(message.chat.id, GIF)
+    GIF = open('./data/tldr.mp4', 'rb')
+    bot.send_document(message.chat.id, GIF)
 
 # Only admins!!
+
+
 @bot.message_handler(commands=['update'])
 def auto_update(message):
- if isAdmin_fromPrivate(message):
-   bot.reply_to(message, "Reiniciando..\n\nPrueba algun comando en 10 segundos")
-   print("Updating..")
-   sys.exit()
- else:
-   bot.reply_to(message, "Este comando es solo para admins y debe ser enviado por privado")
+    if isAdmin_fromPrivate(message):
+        bot.reply_to(message, "Reiniciando..\n\nPrueba algun comando en 10 segundos")
+        print("Updating..")
+        sys.exit()
+    else:
+        bot.reply_to(message, "Este comando es solo para admins y debe ser enviado por privado")
+
 
 @bot.message_handler(commands=['newreto'])
 def new_challenge(message):
-  if isAdmin_fromPrivate(message):
-    url_reto = message.text.split(' ', 1)[1]
-    j['reto'] = url_reto
-    
-    with open('./data/data.json', 'w') as dataW:
-      dataW.write(json.dumps(j))
-      
-    bot.reply_to(message, "El nuevo reto es:\n\n" + url_reto)
-    print("Updating..")
-    sys.exit()
-  else:
-    bot.reply_to(message, "Este comando es solo para admins y debe ser enviado por privado")
+    if isAdmin_fromPrivate(message):
+        url_reto = message.text.split(' ', 1)[1]
+        j['reto'] = url_reto
+        with open('./data/data.json', 'w') as dataW:
+            dataW.write(json.dumps(j))
+        bot.reply_to(message, "El nuevo reto es:\n\n" + url_reto)
+        print("Updating..")
+        sys.exit()
+    else:
+        bot.reply_to(message, "Este comando es solo para admins y debe ser enviado por privado")
 
 
 #@bot.message_handler(commands=['spam'])
 #def send_spam(message):
 #  if message.chat.type == 'private':
 #    if str(message.from_user.id) in admins.keys():
-#      text = message.text.split(' ', 1)[1]      
+#      text = message.text.split(' ', 1)[1]
 #      bot.send_message("@theIronChannel", text)
 #      r = api.request('statuses/update', {'status': text})
 #      print(r.status_code)
